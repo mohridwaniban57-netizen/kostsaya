79a21c189c79fb68310e7eb3d702dfacc1f374a0
rezil albuchori <rezilgaming5@gmail.com>
2026-05-18 18:13:27 +0800

    saya baru melakukan perubahan di folder view

---
+++ app/Views/Layout/footer.php
@@ -0,0 +1,6 @@
+<footer class="footer">
+    <p>© 2026 Sistem Kost | Dibuat dengan CodeIgniter 4</p>
+</footer>
+
+</body>
+</html>
---
+++ app/Views/Layout/template.php
@@ -0,0 +1,13 @@
+<?= $this->include('layout/header'); ?>
+
+<div class="container">
+
+    <h2>Selamat Datang di Sistem Kost</h2>
+
+    <p>
+        Website ini digunakan untuk mencari dan mengelola data kost.
+    </p>
+
+</div>
+
+<?= $this->include('layout/footer'); ?>
