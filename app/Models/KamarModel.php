1c87b429ea08cedf0893f41951cfc9533c2508aa
nazlah_qurani <nazlahaya06@gmail.com>
2026-06-02 15:47:06 +0800

    ini untuk models

--- app/Models/KamarModel.php
+++ app/Models/KamarModel.php
@@ -6,10 +6,22 @@ use CodeIgniter\Model;
 
 class KamarModel extends Model
 {
-    protected $table = 'kamar';
+       protected $table = 'kamar';
     protected $primaryKey = 'kamar_id';
-
-    protected $allowedFields = ['kos_id', 'nomor', 'harga', 'status'];
+    protected $useAutoIncrement = true;
+    protected $returnType = 'array';
+    protected $useSoftDeletes = true;
+    protected $protectFields= true;
+    protected $allowedFields = [
+        'nomor',
+        'harga',
+        'status'
+    ];
 
     protected $useTimestamps = true;
+    protected $dateFormat = 'datetime';
+    protected $createdField = 'created_at';
+    protected $updatedField = 'updated_at';
+    protected $deletedField = 'deleted_at';
+    
 }
--- app/Models/KosModel.php
+++ app/Models/KosModel.php
@@ -8,8 +8,21 @@ class KosModel extends Model
 {
     protected $table = 'kos';
     protected $primaryKey = 'kos_id';
-
-    protected $allowedFields = ['nama_kos', 'pemilik_id', 'lokasi', 'harga', 'foto'];
+    protected $useAutoIncrement = true;
+    protected $returnType = 'array';
+    protected $useSoftDeletes = true;
+    protected $protectFields= true;
+    protected $allowedFields = [
+        'nama_kos',
+        'lokasi',
+        'harga',
+        'foto'
+    ];
 
     protected $useTimestamps = true;
+    protected $dateFormat = 'datetime';
+    protected $createdField = 'created_at';
+    protected $updatedField = 'updated_at';
+    protected $deletedField = 'deleted_at';
+    
 }
