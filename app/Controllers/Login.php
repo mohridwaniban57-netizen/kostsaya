b69e5c46ae7927e2a944126ae8670cbff99eabac
Suci <lixiaa319@gmail.com>
2026-06-05 23:05:28 +0800

    login sudah

--- app/Controllers/Login.php
+++ app/Controllers/Login.php
@@ -4,12 +4,25 @@ namespace App\Controllers;
 
 class Login extends BaseController
 {
-   public function login() {
-        echo view('auth/login');
+    // tampilkan halaman login
+    public function index()
+    {
+        return view('auth/login');
+    }
+
+    // proses login
+    public function proses()
+    {
+        // nanti di sini biasanya cek username & password
+        // contoh sederhana:
+
+        $session = session();
+
+        $session->set([
+            'logged_in' => true
+        ]);
+
+        // arahkan ke dashboard
+        return redirect()->to('/dashboard');
     }
 }
-
-//     public function proses() {
-//         return redirect()->to('admin');
-//     }
-// }
